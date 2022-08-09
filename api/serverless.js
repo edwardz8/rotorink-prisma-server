2cf17d9cdb45369a74933e687c9288e44982c6c0
edwardz8 <tolklorist@gmail.com>
2022-08-09 19:57:06 -0400

    init

--- api/serverless.js
+++ api/serverless.js
@@ -39,7 +39,7 @@ async function commitToDb(promise) {
 }
 
 // Register your application as a normal plugin.
-// app.register(import("../server"));
+app.register(import("../server"));
 
 export default async (req, res) => {
     await app.ready();
