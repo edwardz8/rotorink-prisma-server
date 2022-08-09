e6fa74baa7659d373389b4d1a9eea045ff1630ee
edwardz8 <tolklorist@gmail.com>
2022-08-09 19:53:15 -0400

    init

---
+++ api/serverless.js
@@ -0,0 +1,47 @@
+"use strict";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// Require the framework
+import Fastify from "fastify";
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+});
+
+
+app.register(sensible)
+app.register(cors, {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+})
+
+const prisma = new PrismaClient()
+
+app.get('/players', async (req, res) => {
+   return await commitToDb(prisma.player.findMany({ select: {
+        id: true,
+        name: true,
+        team: true, 
+        goals: true, 
+        assists: true, 
+        fanpoints: true 
+    }
+ }))
+})
+
+async function commitToDb(promise) {
+   const [error, data] = await app.to(promise)
+   if (error) return app.httpErrors.internalServerError(error.message)
+   return data 
+}
+
+// Register your application as a normal plugin.
+// app.register(import("../server"));
+
+export default async (req, res) => {
+    await app.ready();
+    app.server.emit('request', req, res);
+}
